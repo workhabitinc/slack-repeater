200eb31cee8beb4684dc1a0e5bef6afe603e241f
Gary Gogick <gary@workhabit.com>
2014-09-18 00:30:46 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 slack-repeater
 ==============
 
-A repeater for posting messages to Slack channels via the Slack API
+A repeater for posting messages to Slack channels via the Slack API.  SSL is used for a minty fresh secure feeling that fights plaque and lasts all day.
+
+### Installation
+
+Installation should be performed under a normal user with access to node.js via nvm.
+
+After checkout, install the required modules:
+
+```
+npm install express
+npm install body-parser
+npm install request
+```
+
+Next, generate a SSL certificate.
+
+```
+openssl rsa -in old.pem -out key.pem
+openssl req -new -key key.pem -out csr
+openssl x509 -req -days 999 -in csr -signkey key.pem -out cert.pem
+rm old.pem
+rm csr
+```
+
+Configure the Slack endpoint in repeater.js.  This is the 'var url' line, and should look something like the following:
+
+```
+var url = 'https://wh.slack.com/services/hooks/incoming-webhook?token=IJF2i0fm2ofmiHFSHUFH';
+```
+
+The URL and token will be given to you during Slack API setup.
+
+Install the systemd startup script, located at scripts/slack-repeater.service.  This should be located under /etc/systemd/system.  Note that you should change the parameters within the script accordingly to take into account the user the repeater is being installed under.
+
+Finally, start it up.
+
+```
+systemctl enable slack-repeater
+systemctl start slack-repeater
+```
+
+### Testing and Use
+
+You can use the repeater by posting a payload including a channel, username, text, and icon_emoji.  icon_emoji is optional, but the other fields are required.  Example:
+
+```
+curl -kX POST -H "Content-Type: application/json" -d '{"channel": "#general", "username": "j.js", "text": "What?  j.pl is evolving.  j.pl has evolved into j.js.", "icon_emoji": ":coffee:"}' https://127.0.0.1:8000
+```
---
+++ repeater.js
@@ -0,0 +1,73 @@
+// This is a simple "repeater" type job that takes some input, does some
+// checking, and spews it over to Slack.  The idea here is that we can
+// just spew notifications to one place, and have one Slack key to
+// manage, rather than managing things for every single last node/other
+// application we write.
+//
+// SSL is used for that minty fresh secure feeling.
+
+// Setup for our server
+var fs = require('fs');
+var https = require('https');
+var privateKey  = fs.readFileSync('key.pem', 'utf8');
+var certificate = fs.readFileSync('cert.pem', 'utf8');
+var bodyParser = require('body-parser');
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+app.use(bodyParser.json());
+var httpsServer = https.createServer(credentials, app);
+
+// Setup for our posting to Slack
+// Change url as necessary, ensuring token is included
+var request = require('request');
+var url = '';
+
+// Just a brief message if someone accidently GETs /
+app.get('/', function(req, res){
+	res.status(200).send('<h3>Slack Repeater</h3><p>You need to POST, man.  Cry havoc, and let slip the notifications for Slack!</p>');
+});
+
+// Handle POST to /
+app.post('/', function(req, res){
+	var emoji = req.body.icon_emoji;
+	if(!req.body.channel) {
+		res.status(400).send("Message rejected - missing 'channel' parameter.");
+	} else if (!req.body.username) {
+		req.status(400).send("Message rejected - missing 'username' parameter.");
+	} else if (!req.body.text) {
+		req.status(400).send("Message rejected - missing 'text' parameter.");
+	} else {
+		if(!emoji) {
+			emoji = ':ghost:';
+		} 
+		var data = {
+			channel: req.body.channel,
+			username: req.body.username,
+			text: req.body.text,
+			icon_emoji: emoji
+		}
+		request.post(url, {
+			form: {
+				payload: JSON.stringify(data)
+			}
+		}, function(err, response) {
+			// We could probably use some better error logging here.
+			if(err) {
+				console.log(err);
+			}
+			if (response.body !== 'ok') {
+				console.log(err);
+			}
+		});		
+		res.status(200).send('Accepted!');
+	}
+});
+
+// Handle 404
+app.use(function(req, res, next){
+	res.status(404).send("<h3>Invalid page.</h3><p>Your page ain't here, man.  Your page ain't here.</p>");
+});
+
+// Start up our server
+httpsServer.listen(8000);
