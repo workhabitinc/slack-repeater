76ac4264722dc1c1294364d598da2332b8476e5f
Gary Gogick <gary@aug.io>
2016-04-09 13:24:23 -0700

    Opening the proverbial source.

--- README.md
+++ README.md
@@ -1,37 +1,36 @@
 slack-repeater
 ==============
 
-A repeater for posting messages to Slack channels via the Slack API.  SSL is used for a minty fresh secure feeling that fights plaque and lasts all day.
+A repeater microservice for posting messages to Slack channels via the Slack 
+API.  SSL is used for a minty fresh secure feeling, that fights plaque and lasts all day.
+
+This is intended to be used on an internal network, ergo, no authentication is used by the repeater.  Be aware.
 
 ### Installation
 
-Installation should be performed under a normal user with access to node.js via nvm.
+Installation should be performed under a normal user with access to node.js, ideally via nvm.
 
 After checkout, install the required modules:
 
 ```
-npm install express
-npm install body-parser
-npm install request
+npm install
 ```
 
 Next, generate a SSL certificate.
 
 ```
-openssl rsa -in old.pem -out key.pem
-openssl req -new -key key.pem -out csr
-openssl x509 -req -days 999 -in csr -signkey key.pem -out cert.pem
-rm old.pem
-rm csr
+openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem -days 365
 ```
 
 Configure the Slack endpoint in repeater.js.  This is the 'var url' line, and should look something like the following:
 
 ```
-var url = 'https://wh.slack.com/services/hooks/incoming-webhook?token=IJF2i0fm2ofmiHFSHUFH';
+var url = 'https://hooks.slack.com/services/<STUFF>/<MORESTUFF>/<EVENMORESTUFF>';
 ```
 
-The URL and token will be given to you during Slack API setup.
+The full URL string will be given to you when you set up an incoming webhook for Slack.
+
+You can also adjust the port if necessary; it defaults to 8000.
 
 Install the systemd startup script, located at scripts/slack-repeater.service.  This should be located under /etc/systemd/system.  Note that you should change the parameters within the script accordingly to take into account the user the repeater is being installed under.
 
@@ -47,5 +46,5 @@ systemctl start slack-repeater
 You can use the repeater by posting a payload including a channel, username, text, icon_emoji and attachments array.  icon_emoji and attachments are optional, but the other fields are required.  Example:
 
 ```
-curl -kX POST -H "Content-Type: application/json" -d '{"channel": "#general", "username": "j.js", "text": "What?  j.pl is evolving.  j.pl has evolved into j.js.", "icon_emoji": ":coffee:"}' https://127.0.0.1:8000
+curl -kX POST -H "Content-Type: application/json" -d '{"channel": "quicktest", "username": "SlackRepeater", "text": "Test message, please ignore.", "icon_emoji": ":coffee:"}' https://127.0.0.1:8000
 ```
--- repeater.js
+++ repeater.js
@@ -1,10 +1,31 @@
-// This is a simple "repeater" type job that takes some input, does some
-// checking, and spews it over to Slack.  The idea here is that we can
-// just spew notifications to one place, and have one Slack key to
-// manage, rather than managing things for every single last node/other
-// application we write.
+// Copyright (c) 2016 - WorkHabit, Inc.
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
 //
-// SSL is used for that minty fresh secure feeling.
+// 1. Redistributions of source code must retain the above copyright notice, 
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation 
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// POSSIBILITY OF SUCH DAMAGE.
 
 // Setup for our server
 var fs = require('fs');
@@ -17,6 +38,7 @@ var express = require('express');
 var app = express();
 app.use(bodyParser.json());
 var httpsServer = https.createServer(credentials, app);
+var port = 8000;
 
 // Setup for our posting to Slack
 // Change url as necessary, ensuring token is included
@@ -56,9 +78,10 @@ app.post('/', function(req, res){
 		}, function(err, response) {
 			// We could probably use some better error logging here.
 			if(err) {
-				console.log(err);
+				console.log("Error: " + err);
 			}
 			if (response.body !== 'ok') {
+				console.log("Error from Slack: " + response.body);
 				console.log(err);
 			}
 		});		
@@ -72,4 +95,4 @@ app.use(function(req, res, next){
 });
 
 // Start up our server
-httpsServer.listen(8000);
+httpsServer.listen(port);
